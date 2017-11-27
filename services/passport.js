b41a0b079d48b1a0fc5f5a9b9471470970a6e04a
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-11-26 18:01:14 -0800

    enabled editting on search page

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,10 @@ const passport       = require('passport'),
 
 const User = require('../models/User')
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+passport.serializeUser((user, done) => done(null, user.id));
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
-    done(null, user);
-  });
+  User.findById(id).then(user => done(null, user));
 });
 
 passport.use(
